b9e32ed454b69906e2743801a9dd9899e7bcdf4c
Shubom123 <104338268+Shubom123@users.noreply.github.com>
2022-07-12 15:00:30 +0530

    Added: 2D array functionality added

---
+++ array2d/Main.java
@@ -0,0 +1,11 @@
+package com.array2d;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PrimeTwoDim primeNo = new PrimeTwoDim();
+        primeNo.primeNo();
+        primeNo.show();
+    }
+
+}
