78b76c3239b6e2473aced95c0862afb62dd75b6b
Shubom123 <104338268+Shubom123@users.noreply.github.com>
2022-07-12 15:23:27 +0530

    Added: Data structure program added

---
+++ palindromechecker/Node.java
@@ -0,0 +1,13 @@
+package com.palindromechecker;
+
+public class Node {
+
+    Node next;
+    String letter;
+    public Node(String letter) {
+        super();
+        this.next =null;
+        this.letter = letter;
+    }
+
+}
