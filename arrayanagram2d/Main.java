1b7143589e33800cd1c3584e77d089f643cf2e83
Shubom123 <104338268+Shubom123@users.noreply.github.com>
2022-07-12 15:04:34 +0530

    Added: Data structures program added

---
+++ arrayanagram2d/Main.java
@@ -0,0 +1,12 @@
+package com.arrayanagram2d;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PrimeAnagram primeAnagram = new PrimeAnagram();
+        primeAnagram.anagramNo();
+        primeAnagram.show();
+    }
+}
