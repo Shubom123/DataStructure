d7b6ed73dd819a7290b80e0c30963778c483269b
Shubom123 <104338268+Shubom123@users.noreply.github.com>
2022-07-12 15:10:00 +0530

    Added: Data structures program added

---
+++ arry2dstack/Main.java
@@ -0,0 +1,11 @@
+package com.arry2dstack;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack stack = new Stack();
+        stack.primeAnagramPrint();
+    }
+}
